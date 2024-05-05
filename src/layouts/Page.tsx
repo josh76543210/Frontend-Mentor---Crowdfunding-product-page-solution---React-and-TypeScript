463ff9526a54980b673317e2168d13f8799a00ae
Josh <119835493+josh76543210@users.noreply.github.com>
2024-05-04 21:56:57 -0700

    Add background image and google font with tailwind;

--- src/layouts/Page.tsx
+++ src/layouts/Page.tsx
@@ -1,5 +1,9 @@
 function Page({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div className="font-commissioner min-h-screen bg-hero-mobile sm:bg-hero-desktop bg-no-repeat bg-contain flex flex-col">
+      {children}
+    </div>
+  );
 }
 
 export default Page;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-desktop": "url('./src/assets/images/image-hero-desktop.jpg')",
+        "hero-mobile": "url('./src/assets/images/image-hero-mobile.jpg')",
+      },
+    },
+    fontFamily: {
+      commissioner: ["Commissioner", "sans-serif"],
+    },
   },
   plugins: [],
 };
