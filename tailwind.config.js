870505bddcd0eb1fdc21fcfe39c6a0e04fc8dcce
Josh <119835493+josh76543210@users.noreply.github.com>
2024-05-04 22:00:08 -0700

    Add custom colors to tailwind;

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,12 @@ export default {
     fontFamily: {
       commissioner: ["Commissioner", "sans-serif"],
     },
+    colors: {
+      "moderate-cyan": "hsl(176, 50%, 47%)",
+      "dark-cyan": "hsl(176, 72%, 28%)",
+      black: "hsl(0, 0%, 0%)",
+      "dark-gray": "hsl(0, 0%, 48%)",
+    },
   },
   plugins: [],
 };
