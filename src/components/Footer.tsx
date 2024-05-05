91542f65ff583f9412fda31a74a0706d5fedd779
Josh <119835493+josh76543210@users.noreply.github.com>
2024-05-04 23:31:00 -0700

    Add links to Footer;

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,27 @@
 function Footer() {
   return (
     <footer className="flex flex-col gap-4 items-center justify-center mt-auto p-8 sm:flex-row sm:gap-2">
-      <span>Challenge by Frontend Mentor</span>
+      <span>
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io/?ref=challenge"
+          target="_blank"
+          className="text-moderate-cyan hover:text-dark-cyan"
+        >
+          Frontend Mentor
+        </a>
+      </span>
       <span className="hidden sm:inline">│</span>
-      <span>Coded by josh76543210 </span>
+      <span>
+        Coded by{" "}
+        <a
+          href="https://www.frontendmentor.io/profile/josh76543210"
+          target="_blank"
+          className="text-moderate-cyan hover:text-dark-cyan"
+        >
+          josh76543210
+        </a>
+      </span>
     </footer>
   );
 }
