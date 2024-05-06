642fb63e69a7c0ba8f38f1c99c73116b824e88c3
Josh <119835493+josh76543210@users.noreply.github.com>
2024-05-06 01:29:52 -0700

    Add color gradient for background page image;

--- src/layouts/Page.tsx
+++ src/layouts/Page.tsx
@@ -1,6 +1,19 @@
 function Page({ children }: { children: React.ReactNode }) {
   return (
-    <div className="font-commissioner min-h-screen bg-hero-mobile sm:bg-hero-desktop bg-no-repeat bg-contain flex flex-col px-6 bg-slate-50">
+    <div
+      className="font-commissioner min-h-screen bg-hero-mobile sm:bg-hero-desktop bg-no-repeat bg-contain flex flex-col px-6 bg-slate-50 relative z-10 
+      before:content-['']
+      before:absolute
+      before:inset-0
+      before:block
+      before:bg-gradient-to-b
+      before:from-black
+      before:from-5%
+      before:to-transparent
+      before:to-30%
+      before:opacity-75
+      before:z-[-5]"
+    >
       {children}
     </div>
   );
